2e10291447963ef25999e8544750634ee214d05f
erguotou <erguotou525@gmail.com>
2017-09-06 13:38:37 +0800

    change to travis ci cron job

--- bee.go
+++ bee.go
@@ -3,15 +3,12 @@ package main
 import (
 	// "crypto/tls"
 	"encoding/json"
-	"github.com/robfig/cron"
 	"net/url"
 	// "gopkg.in/gomail.v2"
 	"io/ioutil"
 	"log"
-	"math"
 	"net/http"
 	"os"
-	"strconv"
 )
 
 // Oil 油价
@@ -41,29 +38,26 @@ type ResponseBody struct {
 }
 
 var (
-	logFilePath = "logs/error.log"
-	debugLog    *log.Logger
-	apiURL      string
-	serverURL   string
+	apiURL    string
+	serverURL string
 )
 
 func request(url string) (*ResponseBody, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		debugLog.Println("Request error: " + err.Error())
+		log.Println("Request error: " + err.Error())
 		return &ResponseBody{}, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		debugLog.Println("Request parse error: " + err.Error())
+		log.Println("Request parse error: " + err.Error())
 		return &ResponseBody{}, err
 	}
-	log.Print(string(body))
 	var resBody ResponseBody
 	err = json.Unmarshal(body, &resBody)
 	if err != nil {
-		debugLog.Println("Request convert json error: " + err.Error())
+		log.Println("Request convert json error: " + err.Error())
 		return &ResponseBody{}, err
 	}
 	return &resBody, nil
@@ -89,7 +83,7 @@ func request(url string) (*ResponseBody, error) {
 func sendServer(url string, data map[string][]string) error {
 	resp, err := http.PostForm(url, data)
 	if err != nil {
-		debugLog.Println("Send wechat error: " + err.Error())
+		log.Println("Send wechat error: " + err.Error())
 		return err
 	}
 	defer resp.Body.Close()
@@ -107,51 +101,14 @@ func main() {
 		log.Fatal("Need api url and server url")
 		os.Exit(-1)
 	}
-	// 确保日志文件存在
-	var err error
-	var file *os.File
-	var prevOilPrice float64
-	file, err = os.Create(logFilePath)
+	resBody, err := request(apiURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return
 	}
-	debugLog = log.New(file, "[Debug]", log.LstdFlags)
-	debugLog.Println("Start to record")
-	c := cron.New()
-	c.AddFunc("@daily", func() {
-		resBody, err := request(apiURL)
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		price, _ := strconv.ParseFloat(resBody.ResBody.List[0].P92, 10)
-		if prevOilPrice != 0 {
-			if price != prevOilPrice {
-				var str string
-				if price > prevOilPrice {
-					str = "上调了"
-				} else {
-					str = "下降了"
-				}
-				var diff = strconv.FormatFloat(math.Abs(price-prevOilPrice), 'g', 10, 64)
-				prevOilPrice = price
-				// sendMail("今日油价:" + resBody.ResBody.List[0].P92 + "，同比" + str + diff + "。")
-				sendServer(serverURL, url.Values{
-					"text": {"油价又调啦"},
-					"desp": {"今日油价:" + resBody.ResBody.List[0].P92 + "，同比" + str + diff + "。"},
-				})
-			}
-		} else {
-			prevOilPrice = price
-			// sendMail("今日油价:" + resBody.ResBody.List[0].P92 + "。")
-			sendServer(serverURL, url.Values{
-				"text": {"油价又调啦"},
-				"desp": {"今日油价:" + resBody.ResBody.List[0].P92 + "。"},
-			})
-		}
+	// sendMail("今日油价:" + resBody.ResBody.List[0].P92 + "。")
+	sendServer(serverURL, url.Values{
+		"text": {"今日油价"},
+		"desp": {resBody.ResBody.List[0].P92 + "元。"},
 	})
-	c.Start()
-	defer c.Stop()
-	sendServer(serverURL, url.Values{"text": {"服务启动啦"}, "desp": {"油价监听服务启动啦~"}})
-	select {}
 }
